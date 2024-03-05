6f9c1fb9b5c0422c3a22edd33756cae488930fc5
Jared Henderson <jaredh159@users.noreply.github.com>
2024-03-05 14:58:36 -0500

    basic test of doc pages w/o fallback

--- app/[friend]/[document]/page.tsx
+++ app/[friend]/[document]/page.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 
 type Params = {
-  friend_slug: string;
-  document_slug: string;
+  friend: string;
+  document: string;
 };
 
 export async function generateStaticParams(): Promise<Params[]> {
   return [
-    { friend_slug: `george-fox`, document_slug: `journal` },
-    { friend_slug: `ann_branson`, document_slug: `writings` },
+    { friend: `george-fox`, document: `journal` },
+    { friend: `ann_branson`, document: `writings` },
   ];
 }
 
-async function getData(params: {
-  friend_slug: string;
-  document_slug: string;
-}): Promise<string> {
-  return `Some data for ${params.friend_slug}/${params.document_slug}`;
+async function getData(params: { friend: string; document: string }): Promise<string> {
+  return `Some data for ${params.friend}/${params.document}`;
 }
 
 export default async function Page(props: {
